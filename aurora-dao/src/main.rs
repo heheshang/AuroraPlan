1226d13d5604d829eab1702f892bf605f789f9ed
ssk <shang.shi.kun@163.com>
2023-11-29 17:30:53 +0800

    add pagination

--- aurora-dao/src/main.rs
+++ aurora-dao/src/main.rs
@@ -1,5 +1,10 @@
+use diesel::{Connection, PgConnection};
+
 pub mod pagination;
 pub mod schema;
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let database_url = dotenvy::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let _conn = &mut PgConnection::establish(&database_url)?;
     println!("Hello, world!");
+    Ok(())
 }
