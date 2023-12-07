56c5f5fa63c87089206d3d440746c078c1647d91
ssk <shang.shi.kun@163.com>
2023-12-07 17:30:21 +0800

    reove used

--- aurora-entity/gen_entity.sh
+++
@@ -1,3 +0,0 @@
-#!bin/bash
-# Generate entity files of database `bakery` to `entity/src`
-sea-orm-cli generate entity -u postgres://root:root@127.0.0.1:5432/dolphinscheduler --with-serde both --date-time-crate chrono --lib -o  ./src
